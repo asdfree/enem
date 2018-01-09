1b1325cf642ff5c633e8ea58148fe9b5e56f5f81
Anthony Damico <ajdamico@gmail.com>
2018-01-09 14:19:58 -0500

    acs bsapuf

---
+++ tests/setup.R
@@ -0,0 +1,137 @@
+if ( .Platform$OS.type == 'windows' ) memory.limit( 256000 )
+
+library(lodown)
+lodown( "enem" , output_dir = file.path( getwd() ) )
+library(lodown)
+# examine all available ENEM microdata files
+enem_cat <-
+	get_catalog( "enem" ,
+		output_dir = file.path( getwd() ) )
+
+# 2015 only
+enem_cat <- subset( enem_cat , year == 2015 )
+# download the microdata to your local computer
+
+library(DBI)
+dbdir <- file.path( getwd() , "SQLite.db" )
+db <- dbConnect( RSQLite::SQLite() , dbdir )
+
+dbSendQuery( db , "ALTER TABLE microdados_enem_2015 ADD COLUMN female INTEGER" )
+
+dbSendQuery( db , 
+	"UPDATE microdados_enem_2015 
+	SET female = 
+		CASE WHEN tp_sexo = 2 THEN 1 ELSE 0 END" 
+)
+
+dbSendQuery( db , "ALTER TABLE microdados_enem_2015 ADD COLUMN fathers_education INTEGER" )
+
+dbSendQuery( db , 
+	"UPDATE microdados_enem_2015 
+	SET fathers_education = 
+		CASE WHEN q001 = 1 THEN '01 - nao estudou'
+			WHEN q001 = 2 THEN '02 - 1 a 4 serie'
+			WHEN q001 = 3 THEN '03 - 5 a 8 serie'
+			WHEN q001 = 4 THEN '04 - ensino medio incompleto'
+			WHEN q001 = 5 THEN '05 - ensino medio'
+			WHEN q001 = 6 THEN '06 - ensino superior incompleto'
+			WHEN q001 = 7 THEN '07 - ensino superior'
+			WHEN q001 = 8 THEN '08 - pos-graduacao'
+			WHEN q001 = 9 THEN '09 - nao estudou' ELSE NULL END" 
+)
+dbGetQuery( db , "SELECT COUNT(*) FROM microdados_enem_2015" )
+
+dbGetQuery( db ,
+	"SELECT
+		fathers_education ,
+		COUNT(*) 
+	FROM microdados_enem_2015
+	GROUP BY fathers_education"
+)
+dbGetQuery( db , "SELECT AVG( nota_mt ) FROM microdados_enem_2015" )
+
+dbGetQuery( db , 
+	"SELECT 
+		fathers_education , 
+		AVG( nota_mt ) AS mean_nota_mt
+	FROM microdados_enem_2015 
+	GROUP BY fathers_education" 
+)
+dbSendQuery( db , 
+	"CREATE FUNCTION 
+		div_noerror(l DOUBLE, r DOUBLE) 
+	RETURNS DOUBLE 
+	EXTERNAL NAME calc.div_noerror" 
+)
+dbGetQuery( db , 
+	"SELECT 
+		uf_residencia , 
+		div_noerror( 
+			COUNT(*) , 
+			( SELECT COUNT(*) FROM microdados_enem_2015 ) 
+		) AS share_uf_residencia
+	FROM microdados_enem_2015 
+	GROUP BY uf_residencia" 
+)
+dbGetQuery( db , "SELECT SUM( nota_mt ) FROM microdados_enem_2015" )
+
+dbGetQuery( db , 
+	"SELECT 
+		fathers_education , 
+		SUM( nota_mt ) AS sum_nota_mt 
+	FROM microdados_enem_2015 
+	GROUP BY fathers_education" 
+)
+dbGetQuery( db , "SELECT QUANTILE( nota_mt , 0.5 ) FROM microdados_enem_2015" )
+
+dbGetQuery( db , 
+	"SELECT 
+		fathers_education , 
+		QUANTILE( nota_mt , 0.5 ) AS median_nota_mt
+	FROM microdados_enem_2015 
+	GROUP BY fathers_education" 
+)
+dbGetQuery( db ,
+	"SELECT
+		AVG( nota_mt )
+	FROM microdados_enem_2015
+	WHERE in_presenca_mt = 1"
+)
+dbGetQuery( db , 
+	"SELECT 
+		VAR_SAMP( nota_mt ) , 
+		STDDEV_SAMP( nota_mt ) 
+	FROM microdados_enem_2015" 
+)
+
+dbGetQuery( db , 
+	"SELECT 
+		fathers_education , 
+		VAR_SAMP( nota_mt ) AS var_nota_mt ,
+		STDDEV_SAMP( nota_mt ) AS stddev_nota_mt
+	FROM microdados_enem_2015 
+	GROUP BY fathers_education" 
+)
+dbGetQuery( db , 
+	"SELECT 
+		CORR( CAST( female AS DOUBLE ) , CAST( nota_mt AS DOUBLE ) )
+	FROM microdados_enem_2015" 
+)
+
+dbGetQuery( db , 
+	"SELECT 
+		fathers_education , 
+		CORR( CAST( female AS DOUBLE ) , CAST( nota_mt AS DOUBLE ) )
+	FROM microdados_enem_2015 
+	GROUP BY fathers_education" 
+)
+library(dplyr)
+dplyr_db <- dplyr::src_sqlite( dbdir )
+enem_tbl <- tbl( dplyr_db , 'microdados_enem_2015' )
+enem_tbl %>%
+	summarize( mean = mean( nota_mt ) )
+
+enem_tbl %>%
+	group_by( fathers_education ) %>%
+	summarize( mean = mean( nota_mt ) )
+dbGetQuery( db , "SELECT COUNT(*) FROM microdados_enem_2015" )
